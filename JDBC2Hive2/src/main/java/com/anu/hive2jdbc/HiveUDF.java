38e6daa387a0d5f2ccd10f6d689cf888bebbca43
NiuCuiming <578277507@qq.com>
2017-12-25 23:15:43 +0800

    测试

--- JDBC2Hive2/src/main/java/com/anu/hive2jdbc/HiveUDF.java
+++ JDBC2Hive2/src/main/java/com/anu/hive2jdbc/HiveUDF.java
@@ -4,6 +4,7 @@ public class HiveUDF {
 
     public static void main(String[] args) {
         System.out.println("hello word!");
-        System.out.println("hello Git");
+        System.out.println("hello Git!");
+        System.out.println("I make a change!");
     }
 }
